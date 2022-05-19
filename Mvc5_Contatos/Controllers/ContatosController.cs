50805d83b9e67a96c86cc29cf1b8082fee064b2b
Weslei_Rodrigues <rodrigueslee80@gmail.com>
2022-05-19 14:02:47 -0300

    Consuming a WEB API - POST Method

--- Mvc5_Contatos/Controllers/ContatosController.cs
+++ Mvc5_Contatos/Controllers/ContatosController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
@@ -40,6 +41,36 @@ namespace Mvc5_Contatos.Controllers
             return View(contatos);
             }
            
+        }
+
+        [HttpGet]
+        public ActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create(ContatoEnderecoViewModel contato)
+        {
+            if(contato == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:59216/api/");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<ContatoEnderecoViewModel>("contatos", contato);
+                postTask.Wait();
+                var result = postTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError(string.Empty, "Erro no Servidor. Contate o Administrador.");
+                return View(contato);
+            }
         }
     }
 }
